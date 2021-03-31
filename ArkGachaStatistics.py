87258df768a5bf924378247d51881a402b4f90ca
ngc7331 <ngc7331@outlook.com>
2021-03-31 17:28:50 +0800

    0131

--- ArkGachaStatistics.py
+++ ArkGachaStatistics.py
@@ -5,17 +5,27 @@ import time
 import os
 import argparse
 import matplotlib.pyplot as plt
-import matplotlib.animation as ani
-import matplotlib.gridspec as gs
+import pylab
 rec = []
 olddate = 0
+logfile = 'log'
 
 parser = argparse.ArgumentParser(description='Arknights Gacha Statistics - 一个明日方舟抽卡统计工具', add_help= True)
+parser.add_argument('-d', '--debug', action='store_true', help='输出调试信息.')
+parser.add_argument('-e', '--export', action='store_true', help='直接从已有数据导出图片.')
+parser.add_argument('-f', '--file', metavar='filename', help='设置记录的文件名(默认为log.json).')
 parser.add_argument('-r', '--reset', action='store_true', help='清除历史记录.')
 parser.add_argument('-s', '--skip-fetch', action='store_true', help='跳过从官网更新抽卡数据.')
 parser.add_argument('--skip-draw', action='store_true', help='跳过画图.')
-parser.add_argument('-e', '--export', action='store_true', help='直接从已有数据导出图片.')
 args = parser.parse_args()
+
+# 指定默认字体
+pylab.rcParams['font.sans-serif'] = ['SimHei']
+pylab.rcParams['axes.unicode_minus'] = False
+
+def debug(m):
+    if (args.debug):
+        print('[Debug] %s' % m)
 
 def fetch():
     #初始化浏览器
@@ -77,7 +87,7 @@ def fetch():
     #写入log
     print('Dump json')
     rec.extend(reversed(new_rec))
-    with open('logs/log.json', 'w', encoding='UTF-8') as f:
+    with open('logs/%s.json' % logfile, 'w', encoding='UTF-8') as f:
         f.write(json.dumps(rec, indent=2, separators=(',', ': '), ensure_ascii=False))
     print('Done')
     time.sleep(1)
@@ -87,14 +97,26 @@ def draw():
     print('Draw picture')
     # 统计数据
     total = len(rec)
-    count = [0, 0, 0, 0]
-    trend = [[0], [0], [0], [0]]
+    count = [0, 0, 0, 0] # 3~6星计数
+    trend = [[0], [0], [0], [0]] # 3~6星趋势
+    chars = {} # 5/6星角色计数
+    trend25 = [] #25抽分布
     tmp = [0, 0, 0, 0]
     for i in rec:
-        index = i['rarity']-3
-        count[index] += 1
+        name, rarity = i['name'], i['rarity']
+        count[rarity-3] += 1
         for j in range(4):
-            trend[j].append(trend[j][-1]+1 if index ==  j else trend[j][-1])
+            trend[j].append(trend[j][-1]+1 if rarity-3 ==  j else trend[j][-1])
+        if (rarity >= 5):
+            chars.setdefault(name, 0)
+            chars[name] += 1
+        tmp[rarity-3] += 1
+        if (sum(tmp) == 25 or i == rec[-1]):
+            trend25.append(tmp)
+            tmp = [0, 0, 0, 0]
+    chars = sorted(chars.items(), key=lambda d:d[1], reverse=True)
+    debug(chars)
+    debug(trend25)
     # 画图
     fig, axes = plt.subplots(
         2, 2,
@@ -114,21 +136,35 @@ def draw():
         autopct = lambda x: '%d / %1.2f%%' % (x*total/100, x),
         wedgeprops = dict(edgecolor='black', linewidth=1)
     )
-    axes[0, 0].set_title('total: %d' % total, y=-0.05)
+    axes[0, 0].set_title('总计: %d抽' % total, y=-0.05)
     # 稀有度分布趋势堆积式折线图
+    axes[0, 1].set_title('稀有度分布趋势')
     axes[0, 1].stackplot(
         range(total+1), # x轴
         trend[3], trend[2], trend[1], trend[0], # 折线数据
         labels = reversed(labels), # 倒序排列 -> 高稀有度在下
         colors = reversed(colors)
     )
-    axes[0, 1].set_title('Rarity distribution trend')
     axes[0, 1].legend(loc=2)
-    # 未完成: 5/6星角色出货统计（柱状图） and  每25(暂定)抽的出率统计（柱状图）
-    axes[1, 0].set_title('High rarity character distribution (Unfinished)')
-    axes[1, 1].set_title('Rarity distribution in every 25 draws (Unfinished)')
+    # 高星角色统计
+    axes[1, 0].set_title('5/6星角色统计')
+    axes[1, 0].bar(
+        list(map(lambda l: l[0], [l for l in chars])),
+        list(map(lambda l: l[1], [l for l in chars]))
+    )
+    for tick in axes[1, 0].get_xticklabels(): # 旋转标签
+        tick.set_rotation(45)
+    # 未完成: 每25(暂定)抽的出率统计（柱状图）
+    axes[1, 1].set_title('每25抽稀有度分布')
+    for i in range(4):
+        axes[1, 1].bar(
+            range(len(trend25)),
+            list(map(lambda l: l[i], [l for l in trend25])),
+            bottom = list(map(lambda l: sum(l[i+1:]), [l for l in trend25])),
+            color = colors[i]
+        )
     # 保存&显示
-    filename = rec[-1]['date'].replace(':', '-')
+    filename = '%s %s' % (logfile, rec[-1]['date'].replace(':', '-'))
     if (args.export):
         fig.savefig('logs/%s.jpg' % filename)
         print('Saved to logs/%s.jpg' % filename)
@@ -143,18 +179,22 @@ def draw():
     return None
 
 if (__name__ == '__main__'):
+    logfile = args.file.replace('.json', '') if args.file else 'log'
     try:
-        with open('logs/log.json', 'r', encoding='UTF-8') as f:
+        with open('logs/%s.json' % logfile, 'r', encoding='UTF-8') as f:
             rec = json.load(f)
             olddate = time.mktime(time.strptime(rec[-1]['date'], '%Y-%m-%d %H:%M:%S'))
     except FileNotFoundError:
         if(not os.path.exists('logs')):
             os.mkdir('logs')
+        if (args.skip_fetch or args.export):
+            print('错误：未找到指定的log文件，请检查-f参数，或不使用-s/-e')
+            exit()
     if (args.reset):
         try:
             for root, dirs, files in os.walk('logs', topdown=False):
                 for name in files:
-                    print('删除%s' % os.path.join(root, name))
+                    debug('remove %s' % os.path.join(root, name))
                     os.remove(os.path.join(root, name))
         except:
             print('没有文件需要删除')
--- README.md
+++ README.md
@@ -1,33 +1,47 @@
 # Arknights Gacha Statistics
 一个练手用的明日方舟抽卡统计工具 - 基于python+selenium+chrome
 
-## 基本思路
-1. 用浏览器打开官网页面，手动登录+通过验证码
+## 效果展示
+- 登录
+![](preview/login.png)
+- 自动获取数据
+![](preview/fetch.png)
+- 本地log文件
+![](preview/log.png)
+- 生成图片
+![](preview/export.jpg)
+
+## 基本思路 & BB
+1. 用浏览器打开[官网页面](https://ak.hypergryph.com/user/inquiryGacha)，手动登录+通过验证码
 2. 获取官网抽卡统计表格，获得抽卡时间和角色名，通过分析class获得稀有度，以字典列表的形式存储
 3. 检测下一页是否存在，存在则进入
 4. 我好菜，轻喷qwq
+5. 测试环境：Windows10, python3.6.8, Chrome89.0.4389.90
 
 ## 使用方法
-1. 安装Google Chrome浏览器(89.0.4389.90)，python3
+1. 安装Google Chrome浏览器(89)，python
    - 如果使用Chrome浏览器其他版本(浏览器访问[chrome://settings/help](chrome://settings/help)查看版本号)，请自行[下载对应版本的chromedriver](http://npm.taobao.org/mirrors/chromedriver/)替换本项目文件夹中的`chromedriver.exe`
    - 其他浏览器暂不支持，如果您能适配，欢迎PR
-2. 运行`pip install -r requirements.txt`安装依赖。
-3. 运行`python ArkGachaStatistics.py`执行主程序。~~似乎直接双击即可正常运行？~~ 可选参数：
+2. 命令行运行`pip install -r requirements.txt`安装依赖。
+3. 命令行运行`python ArkGachaStatistics.py`执行主程序。~~似乎直接双击即可正常运行？~~ 可选参数：
 ```
-  usage: ArkGachaStatistics.py [-h] [-r] [-s] [--skip-draw] [-e]
-    -h, --help        show this help message and exit
-    -r, --reset       清除历史记录.
-    -s, --skip-fetch  跳过从官网更新抽卡数据.
-    --skip-draw       跳过画图.
-    -e, --export      直接从已有数据导出图片.
+usage: ArkGachaStatistics.py [-h] [-r] [-s] [--skip-draw] [-e]
+  -h, --help            show this help message and exit
+  -d, --debug           输出调试信息.
+  -e, --export          直接从已有数据导出图片.
+  -f filename, --file filename
+                        设置记录的文件名(默认为log.json).
+  -r, --reset           清除历史记录.
+  -s, --skip-fetch      跳过从官网更新抽卡数据.
+  --skip-draw           跳过画图.
 ```
-4. 输入账户名和密码登录后按回车继续
+4. 输入账户名和密码登录后按回车继续。
+5. **注意**：本程序依赖[官网数据](https://ak.hypergryph.com/user/inquiryGacha)，而它只保存**30日，100条以内**数据，且查询结果可能与游戏内实际操作存在延迟。若您希望获得较为完整的数据，请确保在每个30天，100抽内至少运行一次（程序会自动抛弃重复的数据）
 
 ## Change log
-- 2021-03-29 实现绘制稀有度分布堆叠折线图，实现argparse解析参数
-- 2021-03-28 创建项目，实现获取数据以及绘制稀有度分布饼图
+- 2021-03-31 高星干员分布和每25抽稀有度分布柱状图，中文显示，-f指定log文件名（如果需要获取多个账户的数据请使用此功能）
+- 2021-03-29 绘制稀有度分布堆叠折线图，argparse参数解析
+- 2021-03-28 创建项目，获取数据以及绘制稀有度分布饼图
 
 ## TO-DO
-- 精确到每个干员的分布饼图？
-- 欧气趋势柱状图？（每25抽(暂定)的稀有度分布）
 - ......
